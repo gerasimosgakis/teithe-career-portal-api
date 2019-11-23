b82dc935b9dd7006397f61fda20c6e7b2eea58b1
Gerasimos Gakis <gerasimos.gakis@gmail.com>
2019-11-23 17:57:10 +0000

    add add-like and fix get-posts to get the likes count

---
+++ add-like.js
@@ -0,0 +1,69 @@
+// const db = require("./db_connect");
+// const uuid = require("uuid/v1");
+
+// module.exports.main = async (event, context, callback) => {
+//   const data = JSON.parse(event.body);
+//   data.id = uuid();
+//   console.log(data);
+
+//   try {
+//     const result = await db.insert("likes", data);
+//     return {
+//       statusCode: 200,
+//       body: JSON.stringify({
+//         message: "Like added!" + result,
+//         data
+//       })
+//     };
+//   } catch (e) {
+//     return {
+//       statusCode: e.statusCode || 500,
+//       body: "Could not add like " + e
+//     };
+//   }
+// };
+
+const db = require("./db_connect");
+const uuid = require("uuid/v1");
+
+module.exports.main = async (event, context, callback) => {
+  const data = JSON.parse(event.body);
+  data.id = uuid();
+  console.log(data);
+
+  const sqlCount = `
+    SELECT COUNT(*)
+    FROM likes
+    WHERE likes.user_id = $1
+  `;
+
+  const sqlRemove = `
+    DELETE FROM likes
+    WHERE likes.user_id = $1
+  `;
+
+  try {
+    const queryResult = await db.query(sqlCount, data.user_id);
+    console.log(queryResult[0].count);
+    try {
+      const result =
+        queryResult[0].count <= 0
+          ? await db.insert("likes", data)
+          : db.query(sqlRemove, data.user_id);
+      return {
+        statusCode: 200,
+        body: JSON.stringify({
+          message: "Like added!" + result,
+          data
+        })
+      };
+    } catch (e) {
+      return {
+        statusCode: e.statusCode || 500,
+        body: "Could not add like " + e
+      };
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
--- list-posts.js
+++ list-posts.js
@@ -1,8 +1,9 @@
 // const db = require("./db_connect");
 
 // module.exports.main = async () => {
+//   const sql = "SELECT * FROM posts ORDER BY created_at DESC";
 //   try {
-//     const result = await db.getAll("posts");
+//     const result = await db.query(sql);
 //     return {
 //       statusCode: 200,
 //       body: JSON.stringify(result)
@@ -18,7 +19,14 @@
 const db = require("./db_connect");
 
 module.exports.main = async () => {
-  const sql = "SELECT * FROM posts ORDER BY created_at DESC";
+  const sql = `
+  SELECT p.*, count(l.post_id) AS likes
+  FROM posts p
+  LEFT JOIN likes l
+  ON p.id=l.post_id
+  GROUP BY p.id, l.post_id
+  ORDER BY p.created_at DESC
+  `;
   try {
     const result = await db.query(sql);
     return {
@@ -32,5 +40,3 @@ module.exports.main = async () => {
     };
   }
 };
-
-// const result = await db.query(sql, event.pathParameters.id);
